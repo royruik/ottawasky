8646220b508ae36d7ca585d58f0996e609eec9c8
Roy Rui <yrui053@uottawa.ca>
2024-10-16 20:40:51 -0400

    change router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next'; // Import I18nextProvider
 import i18n from './i18n'; // Import i18n configuration
 
